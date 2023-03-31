3588788bf00887a00553022167a69d1764889af4
Appie <alpay2001@live.nl>
2023-03-31 15:38:31 +0200

    product page almost done, started on filters.

--- Application/Beamer_shop/Beamer_shop/Pages/Shop.cshtml.cs
+++ Application/Beamer_shop/Beamer_shop/Pages/Shop.cshtml.cs
@@ -1,3 +1,6 @@
+using Factory;
+using Logic;
+using Logic.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,8 +9,25 @@ namespace Beamer_shop.Pages
 {
     public class ShopModel : PageModel
     {
+        public string ModelFilter { get; set; }
+        public string MakeFilter { get; set; }
+
+
+        CarService carService = CarFactory.CarService;
+        public List<Product> cars = new List<Product>();
+
+        public ShopModel()
+        {
+            cars.AddRange(carService.GetAllCars());
+        }
+
         public void OnGet()
         {
+            var carFiltered = cars.OfType<Car>().Where(c => c.Make == "2019").ToList();
+            var filteredProducts = cars.Cast<Product>().ToList();
+            cars.Clear();
+            cars.AddRange(filteredProducts);
+
         }
     }
 }
