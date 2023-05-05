d4fae815b2e171fa56d264d53587cdb353c08882
Appie_pc <alpay2001@live.nl>
2023-05-05 18:09:05 +0200

    started on checkout design

---
+++ Application/Beamer_shop/Beamer_shop/Pages/CheckoutInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Beamer_shop.Pages
+{
+    public class CheckoutInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
